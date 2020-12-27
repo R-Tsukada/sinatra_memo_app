d9e9ebe42028c939682e5d1341cd1f25a8bfe681
R-Tsukada <r.tsukada14@gmail.com>
2020-12-27 21:39:01 +0900

    Refactoring do

--- memo_app.rb
+++ memo_app.rb
@@ -2,24 +2,33 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-  def json
-    json_file_path = './memo.json'
-    json = File.open(json_file_path).read
-    json_data = JSON.parse(json)
+=begin
+class MyApp < Sinatra::Base
+  configure :production, :development do
+    enable :logging
   end
 
-  def json_load(json_file_path, json, json_data)
-    @json_file_path = json_file_path
-    @json = json
-    @json_data = json_data
-    @list = @json_data["memos"]
-  end
+def initialize(json_file_path, json, json_data, list)
+  @json_file_path = json_file_path
+  @json = json
+  @json_data = json_data
+  @list = list
+end
+=end
+
+json_file_path = './memo.json'
+json = File.open(json_file_path).read
+json_data = JSON.parse(json)
+list = json_data["memos"]
+
+save = File.open(json_file_path, 'w') do |io|
+  JSON.dump(json_data, io)
+end
+
 
   # ------------------トップページ----------------------
   get '/' do
-    @contents= File.open("memo.json") do |file|
-      JSON.load(file)
-    end
+    @lists = list
     erb :top
   end
 
@@ -31,15 +40,11 @@ require 'json'
   post '/' do
     @title = params[:title]
     @text = params[:text]
-    json_file_path = './memo.json'
-    json = File.open(json_file_path).read
-    json_data = JSON.parse(json)
-    list = json_data["memos"]
-    list.push({ "id" => json_data["memos"].size + 2, "title" => @title, "text" => @text})
 
-    File.open(json_file_path, 'w') do |io|
-      JSON.dump(json_data, io)
-    end
+    list.push({ "id" => json_data["memos"].size + 1, "title" => @title, "text" => @text })
+
+    #ファイルの保存
+    save
 
     redirect to('/')
     erb :post
@@ -48,25 +53,26 @@ require 'json'
   # ------------------メモ詳細----------------------
   get '/:id' do
     @id = params[:id]
-    @contents = File.open("memo.json") do |file|
-      JSON.load(file)
+
+    @lists = list
+    @lists.each do |file|
+      if file["id"] == @id.to_i
+        @title = file["title"]
+        @text = file["text"]
+      end
     end
+
     erb :show
   end
 
   delete '/:id' do
     @id = params[:id]
-    json_file_path = './memo.json'
 
-    json_data = File.open(json_file_path) do |io|
-      JSON.load(io)
-    end
+    @lists = list
+    @lists.delete_at(@id.to_i - 1)
 
-    json_data["memos"].delete_at(@id.to_i - 2)
-
-    File.open(json_file_path, 'w') do |io|
-      JSON.dump(json_data, io)
-    end
+    # ファイルの保存
+    save
 
     redirect to('/')
     erb :top
@@ -75,9 +81,15 @@ require 'json'
   # ------------------メモ編集----------------------
   get '/:id/edit' do
     @id = params[:id]
-    @contents = File.open("memo.json") do |file|
-      JSON.load(file)
+
+    @lists = list
+    @lists.each do |file|
+      if file["id"] == @id.to_i
+        @title = file["title"]
+        @text = file["text"]
+      end
     end
+
     erb :edit
   end
 
@@ -86,15 +98,10 @@ require 'json'
     @memo_title = params[:memo_title]
     @memo_text = params[:memo_text]
 
-    json_file_path = './memo.json'
-    json = File.open(json_file_path).read
-    json_data = JSON.parse(json)
-    list = json_data["memos"]
     list[@id.to_i - 1] = { "id" => @id.to_i, "title" => @memo_title, "text" => @memo_text }
 
-    File.open(json_file_path, 'w') do |io|
-      JSON.dump(json_data, io)
-    end
+    # ファイルの保存
+    save
 
     redirect to('/')
     erb :edit
