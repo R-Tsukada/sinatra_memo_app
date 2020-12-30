f49367826dc105f620240741f2b6c992168b13fb
R-Tsukada <r.tsukada14@gmail.com>
2020-12-30 11:04:02 +0900

    commit

--- memo_app.rb
+++ memo_app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
@@ -5,38 +7,36 @@ require 'json'
 json_file_path = './memo.json'
 json = File.open(json_file_path).read
 json_data = JSON.parse(json)
-list = json_data["memos"]
+list = json_data['memos']
 
-def file_save_to_json(json_file_path, json_data)
+def update_json_file(json_file_path, json_data)
   File.open(json_file_path, 'w') do |io|
     JSON.dump(json_data, io)
   end
 end
 
-def new_file(list, json_data)
-  list.push({ "id" => json_data["memos"].size + 1, "title" => @title, "text" => @text })
+def add_memo_elements_to_json_file(list, json_data)
+  list.push({ 'id' => json_data['memos'].size + 1, 'title' => @title, 'text' => @text })
 end
 
-def change_file(list)
-  list[@id.to_i - 1] = { "id" => @id.to_i, "title" => @memo_title, "text" => @memo_text }
+def change_the_elements_of_json(list)
+  list[@id.to_i - 1] = { 'id' => @id.to_i, 'title' => @memo_title, 'text' => @memo_text }
 end
 
-def file_title_and_text(list)
+def name_and_text_that_matches_id_from_json(list)
   list.each do |file|
-    if file["id"] == @id.to_i
-      @title = file["title"]
-      @text = file["text"]
+    if file['id'] == @id.to_i
+      @title = file['title']
+      @text = file['text']
     end
   end
 end
 
-# ------------------トップページ----------------------
 get '/' do
   @lists = list
   erb :top
 end
 
-# ------------------メモ新規作成----------------------
 get '/new' do
   erb :post
 end
@@ -45,20 +45,18 @@ post '/' do
   @title = params[:title]
   @text = params[:text]
 
-  new_file(list, json_data)
+  add_memo_elements_to_json_file(list, json_data)
 
-  #ファイルの保存
-  file_save_to_json(json_file_path, json_data)
+  update_json_file(json_file_path, json_data)
 
   redirect to('/')
   erb :post
 end
 
-# ------------------メモ詳細----------------------
 get '/:id' do
   @id = params[:id]
 
-  file_title_and_text(list)
+  name_and_text_that_matches_id_from_json(list)
 
   erb :show
 end
@@ -66,21 +64,18 @@ end
 delete '/:id' do
   @id = params[:id]
 
-  @lists = list
-  @lists.delete_at(@id.to_i - 1)
+  list.delete_at(@id.to_i - 1)
 
-  # ファイルの保存
-  file_save_to_json(json_file_path, json_data)
+  update_json_file(json_file_path, json_data)
 
   redirect to('/')
   erb :top
 end
 
-# ------------------メモ編集----------------------
 get '/:id/edit' do
   @id = params[:id]
 
-  file_title_and_text(list)
+  name_and_text_that_matches_id_from_json(list)
 
   erb :edit
 end
@@ -90,12 +85,10 @@ patch '/:id/edit' do
   @memo_title = params[:memo_title]
   @memo_text = params[:memo_text]
 
-  change_file(list)
+  change_the_elements_of_json(list)
 
-  # ファイルの保存
-  file_save_to_json(json_file_path, json_data)
+  update_json_file(json_file_path, json_data)
 
   redirect to('/')
   erb :edit
 end
-#end
