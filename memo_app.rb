849e02f4416997876b4b3f65e4761ad96996cc07
R-Tsukada <r.tsukada14@gmail.com>
2021-01-10 10:30:26 +0900

    Changed the method name.

--- memo_app.rb
+++ memo_app.rb
@@ -5,31 +5,30 @@ require 'sinatra/reloader'
 require 'json'
 require 'pg'
 
-class Memo
-
+class MemoApp
   def initialize(connection)
     @connection = connection
   end
 
   def self.connect_to_sql
-    connection = PG::connect(
-    :host => "localhost",
-    :user => 'ryotsukada',
-    :password => 'Aochan1123',
-    :dbname => 'memo_app'
+    connection = PG.connect(
+      host: 'localhost',
+      user: 'ryotsukada',
+      password: 'Aochan1123',
+      dbname: 'memo_app'
     )
-    Memo.new(connection)
+    MemoApp.new(connection)
   end
 
-  def load_by_sql_data(user_id = nil)
-    results = if user_id
-                @connection.exec_params('SELECT * FROM Memo WHERE user_id = $1 ORDER BY user_id', [user_id])
-              else
-                @connection.exec_params('SELECT * FROM Memo ORDER BY user_id')
-              end
+  def connect_to_sql_data(user_id = nil)
+    sql_data = if user_id
+                 @connection.exec_params('SELECT * FROM Memo WHERE user_id = $1 ORDER BY user_id', [user_id])
+               else
+                 @connection.exec_params('SELECT * FROM Memo ORDER BY user_id')
+               end
     memos = {}
-    results.each do |result|
-      memos[result['user_id']] = { 'memo_title' => result['memo_title'], 'memo_text' => result['memo_text'] }
+    sql_data.each do |datum|
+      memos[datum['user_id']] = { 'memo_title' => datum['memo_title'], 'memo_text' => datum['memo_text'] }
     end
     memos
   end
@@ -53,62 +52,62 @@ class Memo
   def data_edit(memo_title, memo_text, user_id)
     sql = <<~SQL
       UPDATE Memo
-      SET memo_text = $1, memo_title = $2
+      SET memo_title = $1, memo_text = $2
       WHERE user_id = $3
     SQL
     @connection.exec_params(sql, [memo_title, memo_text, user_id])
   end
 end
 
-  get '/' do
-    memo = Memo.connect_to_sql
-    @memo_list = memo.load_by_sql_data
-    erb :top
-  end
+get '/' do
+  memo = MemoApp.connect_to_sql
+  @memo_list = memo.connect_to_sql_data
+  erb :top
+end
 
-  get '/new' do
-    erb :post
-  end
+get '/new' do
+  erb :post
+end
 
-  post '/' do
-    memo_title = params[:memo_title]
-    memo_text = params[:memo_text]
-    memo = Memo.connect_to_sql
-    memo.data_create(memo_title, memo_text)
-    redirect to('/')
-    erb :post
-  end
+post '/' do
+  memo_title = params[:memo_title]
+  memo_text = params[:memo_text]
+  memo = MemoApp.connect_to_sql
+  memo.data_create(memo_title, memo_text)
+  redirect to('/')
+  erb :post
+end
 
-  get '/:id' do
-    @id = params[:id]
-    memo = Memo.connect_to_sql
-    @memo_list = memo.load_by_sql_data(@id)
+get '/:id' do
+  @id = params[:id]
+  memo = MemoApp.connect_to_sql
+  @memo_list = memo.connect_to_sql_data(@id)
 
-    erb :show
-  end
+  erb :show
+end
 
-  delete '/:id' do
-    @id = params[:id]
-    memo = Memo.connect_to_sql
-    memo.data_delete(@id)
-    redirect to('/')
-    erb :top
-  end
+delete '/:id' do
+  @id = params[:id]
+  memo = MemoApp.connect_to_sql
+  memo.data_delete(@id)
+  redirect to('/')
+  erb :top
+end
 
-  get '/:id/edit' do
-    @id = params[:id]
-    memo = Memo.connect_to_sql
-    @memo_list = memo.load_by_sql_data(@id)
+get '/:id/edit' do
+  @id = params[:id]
+  memo = MemoApp.connect_to_sql
+  @memo_list = memo.connect_to_sql_data(@id)
 
-    erb :edit
-  end
+  erb :edit
+end
 
-  patch '/:id/edit' do
-    @id = params[:id]
-    memo_title = params[:memo_title]
-    memo_text = params[:memo_text]
-    memo = Memo.connect_to_sql
-    memo.data_edit(memo_title, memo_text, @id)
-    redirect to('/')
-    erb :edit
-  end
+patch '/:id/edit' do
+  @id = params[:id]
+  memo_title = params[:memo_title]
+  memo_text = params[:memo_text]
+  memo = MemoApp.connect_to_sql
+  memo.data_edit(memo_title, memo_text, @id)
+  redirect to('/')
+  erb :edit
+end
